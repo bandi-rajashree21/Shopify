58e332a238faa2d1a32905c9eefeb20acdf26f19
Bandi Rajashree <bandirs2003@gmail.com>
2026-02-15 19:18:14 +0530

    added product card to product list page

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,5 +1,27 @@
+// CSS import
+import './ProductList.css';
+
+import ProductImage from '../../assets/product.png'
+import ProductBox from '../../components/ProductBox/ProductBox';
+
 function ProductList() {
-    return <>Product list</>
+    return (
+        <div className='container'>
+            <div className='row'>
+                <h2 className='product-list-title text-center'>All Products</h2>
+                <div className='product-list-wrapper d-flex flex-row'>
+
+                    <div className='product-list-box' id='productList'>
+
+                        <ProductBox productImage={ProductImage} name={"dummy"} price={1000} />
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ProductList;
