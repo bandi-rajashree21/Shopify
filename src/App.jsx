60963e34e6321b43ddbb71d8d7f7f957712c7ca4
Bandi Rajashree <bandirs2003@gmail.com>
2026-02-15 18:48:51 +0530

    (feat) : added home and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import Home from './pages/Home/Home'
 
 function App() {
 
   return (
     <>
-      <Header color="light" light={true} expand="md" container="md" fixed="top"/>
+      <Header color="light" light={true} expand="md" container="md" />
+       <Home></Home>
+      <Footer />
     </>
   )
 }
