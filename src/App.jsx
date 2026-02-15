bbb948c0636b5f45d7b906a147b2110eaf8fa3b2
Bandi Rajashree <bandirs2003@gmail.com>
2026-02-15 18:40:19 +0530

    (feat) : added navbar using reactStrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      Shopify
+      <Header color="light" light={true} expand="md" container="md" fixed="top"/>
     </>
   )
 }
