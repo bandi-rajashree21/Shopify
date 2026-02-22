04913b95eead0885cab61c8aa82e71b755027148
Bandi Rajashree <bandirs2003@gmail.com>
2026-02-22 19:15:44 +0530

    (feat) : integrated productList API

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -3,8 +3,21 @@ import './ProductList.css';
 
 import ProductImage from '../../assets/product.png'
 import ProductBox from '../../components/ProductBox/ProductBox';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function ProductList() {
+   const [productList, setProductList] = useState(null);
+
+    async function downloadProducts() {
+        const response = await axios.get(`https://fakestoreapi.com/products`);
+        setProductList(response.data);
+        console.log(response.data);
+    }
+
+    useEffect(() => {
+        downloadProducts();
+    }, []) 
     return (
         <div className='container'>
             <div className='row'>
@@ -13,7 +26,10 @@ function ProductList() {
 
                     <div className='product-list-box' id='productList'>
 
-                        <ProductBox productImage={ProductImage} name={"dummy"} price={1000} />
+                        {productList && productList.map((product)=><ProductBox key={product.id} 
+                                            name={product.title}
+                                            price={product.price}
+                                            productImage={product.image}/>)}
 
                     </div>
 
